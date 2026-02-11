d276260783f82db968e369a40f5cde28f77f1ff7
ItzTJSV2 <ItzTJSV2@gmail.com>
2026-02-11 23:48:20 +0000

    Fix to year filter and searching efficiency

--- python/search.py
+++ python/search.py
@@ -23,15 +23,34 @@ def initialise(MODEL_NAME=MODEL_NAME, INDEX_FILE=INDEX_FILE, ID_FILE=ID_FILE):
 
 def search(query, df, index, ids, model, TOP_K=TOP_K, fromYear=1700, toYear=datetime.now().year, includeUnknown=False):
     results = []
-    while results.count < TOP_K:
-        q = model.encode([query], normalize_embeddings=True)
-        scores, idxs = index.search(q, TOP_K)
-        for i, idx in enumerate(idxs[0]):
+    q = model.encode([query], normalize_embeddings=True)
+
+    # By default, search for 5x the request just in case there are many results that get filtered out
+    scores, idxs = index.search(q, TOP_K*5)
+    for i, idx in enumerate(idxs[0]):
+        try:
             row = df.iloc[ids[idx] - 1]
-            row.fillna(0, inplace=True)
             year = row["year"]
-        if year <= toYear and year >= fromYear and includeUnknown:
+
+            # Checks
+            if not year or str(year).strip() == "":
+                if not includeUnknown:
+                    continue
+            else:
+                # Filters here
+                year = int(year)
+                if year < fromYear or year > toYear:
+                    continue
+            
+            # Add it to the list of results
             results.append((row["title"], row["author"], row["year"], scores[0][i]))
+            
+            if len(results) >= TOP_K:
+                break
+        except Exception as e:
+            print(f"Search error: {e}")
+            break
+
     return results
 
 if __name__ == "__main__":
