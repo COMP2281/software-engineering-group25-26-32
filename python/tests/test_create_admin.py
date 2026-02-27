0f56f1ea70f2fa621d35a81014d02489e253c4c8
piggypiggyyoinkyoink <quackquack904@gmail.com>
2026-02-27 21:39:51 +0000

    added test for client_admin to handle receiving an invalid db path

--- python/tests/test_create_admin.py
+++ python/tests/test_create_admin.py
@@ -6,6 +6,7 @@ import bcrypt
 import pytest
 from unittest.mock import Mock
 
+
 @pytest.fixture(scope="session")
 def test_user_db_path():
     # Create temporary db
@@ -31,7 +32,7 @@ def test_user_db_path():
     os.remove(path)
 
 @pytest.fixture
-def client(test_user_db_path, monkeypatch):
+def create_admin(test_user_db_path, monkeypatch):
     monkeypatch.setenv("USERS_DB_PATH", test_user_db_path)
 
     # Import AFTER env patch
@@ -40,13 +41,13 @@ def client(test_user_db_path, monkeypatch):
     yield create_admin
 
 
-def test_create_admin_success(client):
+def test_create_admin_success(create_admin):
     # Test creating a new admin user
-    result = client.create_admin("NEW_ADMIN", "newpassword")
+    result = create_admin.create_admin("NEW_ADMIN", "newpassword")
     assert result == True
 
     # Check that the new admin user was added to the mock database
-    con = sqlite3.connect(client.DB_PATH)
+    con = sqlite3.connect(create_admin.DB_PATH)
     cur = con.cursor()
     cur.execute("SELECT username FROM Admin WHERE username = ?", ("NEW_ADMIN",))
     row = cur.fetchone()
@@ -55,36 +56,36 @@ def test_create_admin_success(client):
     assert row is not None
     assert row[0] == "NEW_ADMIN"
 
-def test_create_admin_duplicate_username(client):
+def test_create_admin_duplicate_username(create_admin):
     # Test creating an admin user with a duplicate username
-    result = client.create_admin("TEST", "anotherpassword")
+    result = create_admin.create_admin("TEST", "anotherpassword")
     assert result == False
 
-def test_create_admin_missing_fields(client):
+def test_create_admin_missing_fields(create_admin):
     # Test creating an admin user with missing username
-    result = client.create_admin("", "password")
+    result = create_admin.create_admin("", "password")
     assert result == False
 
     # Test creating an admin user with missing password
-    result = client.create_admin("USERNAME", "")
+    result = create_admin.create_admin("USERNAME", "")
     assert result == False
 
-def test_handle_valid_cli_arguments(client, monkeypatch):
+def test_handle_valid_cli_arguments(create_admin, monkeypatch):
     # Mock create_admin so we can just test the passing of CLI arguments
     # We already tested create_admin itself in the previous tests
     mock_create_admin = Mock(return_value=True)
-    monkeypatch.setattr(client, "create_admin", mock_create_admin)
+    monkeypatch.setattr(create_admin, "create_admin", mock_create_admin)
     monkeypatch.setattr(
         sys,
         "argv",
         ["create-admin.py", "NEW_ADMIN_2", "password2", "--db", os.environ.get("USERS_DB_PATH", None)]
     )
-    result = client.main()
+    result = create_admin.main()
     assert result == True
     # check that create_admin was called with the correct arguments
     mock_create_admin.assert_called_once_with("NEW_ADMIN_2", "password2", os.environ.get("USERS_DB_PATH", None))
 
-def test_handle_missing_cli_arguments(client, monkeypatch):
+def test_handle_missing_cli_arguments(create_admin, monkeypatch):
     # Test both missing arguments
     monkeypatch.setattr(
         sys,
@@ -92,7 +93,7 @@ def test_handle_missing_cli_arguments(client, monkeypatch):
         ["create-admin.py", "--db", os.environ.get("USERS_DB_PATH", None)]
     )
     with pytest.raises(SystemExit):
-        result = client.main()
+        result = create_admin.main()
         assert result == False
 
     # Test missing password argument
@@ -102,5 +103,15 @@ def test_handle_missing_cli_arguments(client, monkeypatch):
         ["create-admin.py", "USERNAME_ONLY", "--db", os.environ.get("USERS_DB_PATH", None)]
     )
     with pytest.raises(SystemExit):
-        result = client.main()
+        result = create_admin.main()
         assert result == False
+
+def test_handle_invalid_db_path(create_admin, monkeypatch):
+    # Test with invalid db path
+    monkeypatch.setattr(
+        sys,
+        "argv",
+        ["create-admin.py", "USERNAME", "PASSWORD", "--db", "Z:/invalid/path/to/db.db"]
+    )
+    result = create_admin.main()
+    assert result == False
